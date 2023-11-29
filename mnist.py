2787244de87d8cd7238914960bd6bff389af9a68
SakshamDiwan <sakshamcollege@gmail.com>
2023-11-29 17:21:20 -0500

    Model Trained

--- mnist.py
+++ mnist.py
@@ -1,3 +1,4 @@
+# Importing all the necessary libraries
 import torch
 import torchvision
 from torch import nn
@@ -7,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Model
+# Defining the model
 class FFNN(nn.Module):
     def __init__(self):
         super().__init__()
@@ -17,7 +18,7 @@ class FFNN(nn.Module):
         self.linear_layer4 = nn.Linear(98, 49)
         self.linear_layer5 = nn.Linear(49, 10)
 
-    def forward(self, x):
+    def forward(self, x):  # 5 linear layers with relu activation function
         x = x.view(-1, 784)
         x = F.relu(self.linear_layer1(x))
         x = F.relu(self.linear_layer2(x))
@@ -29,7 +30,7 @@ class FFNN(nn.Module):
 
 model = FFNN()
 
-# Dataset
+# Training Dataset and Dataloader
 train_dataset = torchvision.datasets.MNIST(
     "files/",
     train=True,
@@ -39,8 +40,9 @@ train_dataset = torchvision.datasets.MNIST(
         [lambda x: torch.LongTensor([x]), lambda x: F.one_hot(x, 10)]
     ),
 )
-train_dataloader = DataLoader(train_dataset, batch_size=6, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 
+# Testing Dataset and Dataloader
 test_dataset = torchvision.datasets.MNIST(
     "files/",
     train=False,
@@ -50,18 +52,23 @@ test_dataset = torchvision.datasets.MNIST(
         [lambda x: torch.LongTensor([x]), lambda x: F.one_hot(x, 10)]
     ),
 )
-test_dataloader = DataLoader(test_dataset, batch_size=6, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=True)
 
 
-# Loss Function
+# Cross Entropy Loss (Loss Function)
 cross_entropy_loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+# Adam Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
 num_epochs = 50
 
 
+# Function to train model and back propagate for one epoch
 def train_epoch(model, optimizer, dataloader):
-    model.train()  # You have to tell your model to go into "train" mode
+    model.train()
     losses = []
+    num_correct = 0
     for input, labels in dataloader:
         optimizer.zero_grad()
         output = model(input)
@@ -69,33 +76,46 @@ def train_epoch(model, optimizer, dataloader):
         losses.append(loss.item())
         loss.backward()
         optimizer.step()
-        accuracy = (output.round() == labels).float().mean()
-    print("End of epoch accuracy", float(accuracy))
-    return losses  # Typically you want to keep a list of all the batch losses
+        num_correct += (
+            torch.argmax(output, dim=1) == torch.argmax(torch.squeeze(labels), dim=1)
+        ).sum()
+    print("End of epoch accuracy", float(num_correct / 60000))
+    return losses
 
 
+# Function to test model for one epoch
 def test_epoch(model, dataloader):
     model.eval()
     losses = []
-
+    num_correct = 0
     with torch.no_grad():
         for input, labels in dataloader:
             output = model(input)
             loss = cross_entropy_loss(output, torch.squeeze(labels).float())
             losses.append(loss.item())
-            accuracy = (output.round() == labels).float().mean()
-    print("End of validation epoch accuracy", float(accuracy))
+            num_correct += (
+                torch.argmax(output, dim=1)
+                == torch.argmax(torch.squeeze(labels), dim=1)
+            ).sum()
+    print("End of validation epoch accuracy", float(num_correct / 10000))
     return losses
 
 
+# Lists to store the losses for plotting
 training_epoch_losses = []
-validation_epoch_losses = []
+testing_epoch_losses = []
 
+# Training and testing for {num_epochs} epochs
 for epoch in range(num_epochs):
     training_epoch_losses.append(train_epoch(model, optimizer, train_dataloader))
-    validation_epoch_losses.append(test_epoch(model, test_dataloader))
+    testing_epoch_losses.append(test_epoch(model, test_dataloader))
 
 
+# Plot of the average loss per epoch vs number of epochs
 plt.plot(range(num_epochs), [np.mean(x) for x in training_epoch_losses])
-plt.plot(range(num_epochs), [np.mean(x) for x in validation_epoch_losses])
+plt.plot(range(num_epochs), [np.mean(x) for x in testing_epoch_losses])
+plt.legend(["training", "validation"], loc="upper right")
+plt.xlabel("Number of epochs")
+plt.ylabel("Average loss per epoch")
+plt.title("Loss per epoch")
 plt.show()
